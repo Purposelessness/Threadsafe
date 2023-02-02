4f3a2c39202f274f71ef5ee5cd3946691bc8f23c
Purposelessness <yagodarov.misha.2003@mail.ru>
2023-02-02 07:54:06 +0300

    test: set tests

--- tests/test_lib.cc
+++ tests/test_lib.cc
@@ -4,6 +4,7 @@
 #include <thread>
 
 #include "../map.h"
+#include "../set.h"
 
 template <typename T>
 bool IsNull(const std::optional<T>& a) {
@@ -36,6 +37,10 @@ TEST(Map, MainTest) {
   EXPECT_EQ(IsNull(map["aboba"]), true);
   EXPECT_EQ(map["dassyr"], 20);
   EXPECT_EQ(map["zeleboba"], 5);
+  EXPECT_EQ(map.Erase("zeleboba"), true);
+  EXPECT_EQ(map.Erase("zeleboba"), false);
+  EXPECT_EQ(map.Contains("zeleboba"), false);
+  EXPECT_EQ(map.Contains("dassyr"), true);
 }
 
 TEST(Map, MultithreadingTest) {
@@ -65,6 +70,65 @@ TEST(Map, MultithreadingTest) {
   EXPECT_EQ(map.Contains("zeleboba"), false);
   EXPECT_EQ(map.Contains("@!$RSDF"), false);
   EXPECT_EQ(map.Contains("qwerty"), false);
+}
+
+TEST(Set, MainTest) {
+  Set<std::string> set(2);
+  EXPECT_EQ(set.Contains("aboba"), false);
+  set.Insert("aboba");
+  EXPECT_EQ(set.Contains("aboba"), true);
+  set.Insert("aboba");
+  EXPECT_EQ(set.Contains("aboba"), true);
+
+  EXPECT_EQ(set.Contains("zeleboba"), false);
+  set.Insert("zeleboba");
+  EXPECT_EQ(set.Contains("zeleboba"), true);
+  set.Insert("zeleboba");
+  EXPECT_EQ(set.Contains("zeleboba"), true);
+
+  set.Insert("dassyr");
+  EXPECT_EQ(set.Contains("dassyr"), true);
+  EXPECT_EQ(set.Contains("zeleboba"), true);
+  EXPECT_EQ(set.Contains("aboba"), true);
+
+  EXPECT_EQ(set.Erase("aboba"), true);
+  EXPECT_EQ(set.Erase("aboba"), false);
+  EXPECT_EQ(set.Contains("aboba"), false);
+  EXPECT_EQ(set.Contains("dassyr"), true);
+  EXPECT_EQ(set.Contains("zeleboba"), true);
+  EXPECT_EQ(set.Erase("zeleboba"), true);
+  EXPECT_EQ(set.Erase("zeleboba"), false);
+  EXPECT_EQ(set.Contains("zeleboba"), false);
+  EXPECT_EQ(set.Contains("dassyr"), true);
+}
+
+TEST(Set, MultithreadingTest) {
+  Set<std::string> set{2};
+  set.Insert("aboba");
+  std::vector<std::string> strs{"aboba", "dassyr", "zeleboba", "@!$RSDF",
+                                "qwerty"};
+  std::vector<std::string> strs_del{"zeleboba", "@!$RSDF", "qwerty"};
+  std::vector<std::thread> threads;
+  static constexpr int kThreadsCount = 100;
+  threads.reserve(kThreadsCount);
+  for (int i = 0; i < kThreadsCount; ++i) {
+    threads.emplace_back([&set, strs, strs_del]() {
+      std::for_each(std::make_move_iterator(strs.begin()),
+                    std::make_move_iterator(strs.end()),
+                    [&set](const auto& s) { set.Insert(s); });
+      std::for_each(std::make_move_iterator(strs_del.begin()),
+                    std::make_move_iterator(strs_del.end()),
+                    [&set](const auto& s) { set.Erase(s); });
+    });
+  }
+  for (int i = 0; i < kThreadsCount; ++i) {
+    threads[i].join();
+  }
+  EXPECT_EQ(set.Contains("aboba"), true);
+  EXPECT_EQ(set.Contains("dassyr"), true);
+  EXPECT_EQ(set.Contains("zeleboba"), false);
+  EXPECT_EQ(set.Contains("@!$RSDF"), false);
+  EXPECT_EQ(set.Contains("qwerty"), false);
 }
 
 int main(int argc, char** argv) {
